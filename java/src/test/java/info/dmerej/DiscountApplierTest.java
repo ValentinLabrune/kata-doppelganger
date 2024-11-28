f2cdcd6cb1e468ab4bc6d8856b6c2db9137099d4
Valentin Labrune <valentin.labrune@gmail.com>
2024-11-28 08:12:13 +0100

    mock for discount applier

--- java/src/test/java/info/dmerej/DiscountApplierTest.java
+++ java/src/test/java/info/dmerej/DiscountApplierTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -46,5 +47,27 @@ public class DiscountApplierTest {
     discountApplier.applyV2(10, user);
     assertEquals(1, notifierTest.count);
   }
-
+  @Test
+  void should_notify_twice_when_applying_discount_for_two_users_v1_with_mock() {
+    int count = 0;
+    Notifier notifier = mock(Notifier.class);
+    doReturn(count++).when(notifier).notify(any(), "You've got a new discount of 10%");
+    DiscountApplier discountApplier = new DiscountApplier(notifier);
+    List<User> user = new ArrayList<User>();
+    user.add(Bob);
+    user.add(John);
+    discountApplier.applyV1(10, user);
+    assertEquals(2, count);
+  }
+  @Test
+  void should_notify_twice_when_applying_discount_for_two_users_v2_with_mock() {
+    HashMap<List<User>, Integer> map = new HashMap<>();
+    int count = 0;
+    List<User> user = new ArrayList<User>();
+    user.add(Bob);
+    user.add(John);
+    Notifier notifier = mock(Notifier.class);
+    doReturn(map.put(user, count++)).when(notifier).notify(any(), "You've got a new discount of 10%");
+    assertEquals(1, map.get("Bob"));
+  }
 }
